7809d819fb31765fcfe5dd444fcc48775fb285ce
tgerg <trevorgergics10@gmail.com>
2026-05-31 16:01:17 -0400

    Fix axios baseURL for production pt 2

--- backend/app.py
+++ backend/app.py
@@ -214,7 +214,6 @@ def register():
             )
         return jsonify({"message": "User created"})
     except Exception as e:
-        print(f"REGISTER ERROR: {str(e)}")  # ADD THIS
         return jsonify({"error": f"Registration failed: {str(e)}"}), 500
 
 
